47ec60ede271b0c08d5d0c5a418bf8da9dace9ce
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-09 15:20:02 +0800

    remove tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "@/app/globals.css";
 import NavigationDrawer from "@/components/layout/NavigationDrawer";
 import { Box } from "@mui/material";
 import HeaderOffset from "@/components/layout/HeaderOffset";
